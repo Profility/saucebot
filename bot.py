48095e996821ee0a7345dbfa41d5a9afd92b5680
PROFILE <72250349+Profility@users.noreply.github.com>
2021-12-24 11:31:16 +0800

    Added help command

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import discord
 from ruamel.yaml import YAML
 from pysaucenao import SauceNao
 from discord.ext import commands
+from discord.colour import Color
 from embeds import error_embed, results_embed
 
 with open('config.yml', 'r') as conf:
@@ -27,6 +28,24 @@ saucenao = SauceNao(
     min_similarity=config['saucenao']['min_similarity'],
 )
 
+@bot.command(name="help")
+async def help(ctx):
+    await ctx.reply(
+        embed = discord.Embed(
+            color = Color.lighter_gray(),
+            title="Instructions on how to use SauceBot!",
+            description="""
+            Using SauceBot is very easy and straightforward, you only need to say `s!sauce` along with the URL or the file of the anime image, gif, video.
+
+            Please do keep in mind that results are not always accurate. To check their accuracy, please refer to the similarity percentage.
+
+            **Video Demonstration:**
+            """
+        ).set_image(
+            url = "https://cdn.upload.systems/uploads/kXz4TvKf.gif"
+        )
+    )
+
 @bot.command(name='sauce', aliases=['source', 'saucenao', 'get', 'search'])
 async def sauce(ctx, url: typing.Optional[str]):
     try:
@@ -35,6 +54,11 @@ async def sauce(ctx, url: typing.Optional[str]):
         elif not url:
             if ctx.message.attachments:
                 results = await saucenao.from_url(ctx.message.attachments[0].url)
+            else:
+                await ctx.reply(
+                    embed=await help(ctx)
+                )
+                return
 
         if results:
             try:
@@ -65,7 +89,7 @@ async def sauce(ctx, url: typing.Optional[str]):
                     description = f"""
                     I can't find the source of the image, gif, or video. Maybe the results had low similarity percentage?
 
-                    Please use other ways of finding the source either by reverse image searching or using other source locators like [trace.moe](https://trace.moe) or by creating a post in [r/SauceSharingCommunity](https://www.reddit.com/r/SauceSharingCommunity/).
+                    Please use other ways of finding the source either by reverse image searching or using source locators like [SauceNao](https://saucenao.com/) and [trace.moe](https://trace.moe) or by creating a post in [r/SauceSharingCommunity](https://www.reddit.com/r/SauceSharingCommunity/).
                     """
                 )
             )
