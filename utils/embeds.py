f96f6a58bf2513dad655ed340c9fb6d3c8af2b2b
PROFILE <72250349+Profility@users.noreply.github.com>
2022-01-15 12:18:53 +0800

    Updated "Similarity" and "Help"

--- utils/embeds.py
+++ utils/embeds.py
@@ -18,12 +18,23 @@ def results_embed(database: typing.Optional[str], similarity: typing.Optional[fl
         )
 
     if similarity:
+        sim = f"{similarity}% "
+        if similarity >= 95:
+            sim = sim + "**(Exact)**"
+        elif similarity >= 85.0:
+            sim = sim + "**(High)**"
+        elif similarity >= 70.0:
+            sim = sim + "**(Medium)**"
+        elif similarity >= 60.0:
+            sim = sim + "**(Low)**"
+        else:
+            sim = sim + "**(Very Low)**"
+               
         resultsmessage.add_field(
             name="Similarity:",
-            value=similarity,
+            value=sim,
             inline=False
         )
-
     if author:
         resultsmessage.add_field(
             name="Author:",
@@ -52,9 +63,9 @@ def error_embed(title: str, description: str):
 def help_embed():
     helpmessage = discord.Embed(
         color = Color.lighter_gray(),
-        title="Instructions on how to use SauceBot!",
+        title="🤝   Instructions on how to use SauceBot!",
         description=f"""
-        The command prefix and all the commands are all case insensitive.\n\nUsing SauceBot is very easy and straightforward, you only need to say `{json.loads(config.get("discord", "prefix"))[0]}saucenao` along with the URL or the file of the anime image. In addition, you can also mention someone to get the source of their profile picture.\n\nThere are aliases to this command, like **{', '.join(json.loads(config['saucenao']['aliases']))}**\n\nWhen sending a request, you must use direct image URL. The image must not have unnecessary borders/information, this increases the chances of getting accurate results. SauceBot only picks results with the similarity of **{config.getfloat("saucenao", "min_similarity")}** and above.\n\nPlease do keep in mind that results are not always accurate. To check their accuracy, please refer to the similarity percentage and thumbnail picture.\n\n**Video Demonstration:**
+        Using SauceBot is very easy and straightforward, you only need to say `{json.loads(config.get("discord", "prefix"))[0]}saucenao` along with the URL or the file of the anime image. In addition, you can also mention someone to get the source of their profile picture.\n\nThere are aliases to this command, like **{', '.join(json.loads(config['saucenao']['aliases']))}**\n\nWhen sending a request, you must use direct image URL. The image must not have unnecessary borders/information, this increases the chances of getting accurate results. SauceBot only picks results with the similarity of **{config.getfloat("saucenao", "min_similarity")}** and above.\n\nPlease do keep in mind that results are not always accurate. To check their accuracy, please refer to the similarity percentage and thumbnail picture.\n\n**Video Demonstration:**
         """
     ).set_image(
         url = "https://cdn.upload.systems/uploads/kXz4TvKf.gif"
